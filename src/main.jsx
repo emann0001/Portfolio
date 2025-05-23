25c13c4a191bb9a3eeb9cff61384a65a1d6976df
adenuga emmanuel <ehmmygal@gmail.com>
2025-05-22 20:11:16 -1200

    ssss comit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './app.css';   // <-- Import your Tailwind CSS file here
+import '../style/app.css';   // <-- Import your Tailwind CSS file here
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
